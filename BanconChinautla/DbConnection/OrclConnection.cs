0ef07e662cb5094502b2b4ab5399ec1de7c71a40
Joshuar Najera <joxha11@gmail.com>
2020-03-22 14:07:38 -0600

    oracle connection

--- BanconChinautla/DbConnection/OrclConnection.cs
+++ BanconChinautla/DbConnection/OrclConnection.cs
@@ -8,7 +8,19 @@ namespace BanconChinautla.DbConnection
 {
     public static class OrclConnection
     {
-        
+        public static OracleConnection getConnection()
+        {
+            try
+            {
+                String connString = "Pooling=false;User Id=PROYECTOFINAL_DEV;Password=PROYECTOFINAL_DEV;Data Source=localhost:1521/DBMANAGERS;";
+                return new OracleConnection(connString);
+            }
+            catch (Exception ep)
+            {
+
+            }
+            return null;
+        }
 
     }
 }
---
+++ BanconChinautla/Models/PaisMV.cs
@@ -0,0 +1,42 @@
+﻿using BanconChinautla.DbConnection;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BanconChinautla.Models
+{
+    public class PaisMV
+    {
+        public int COD_PAIS { get; set; }
+        public String DESCRIPCION { get; set; }
+        public String FECHA { get; set; }
+        public string TIPO { get; set; }
+        public char STATUS { get; set; }
+
+        public List<SelectListItem> Paises()
+        {
+            List<SelectListItem> p = new List<SelectListItem>();
+            OracleConnection cp = OrclConnection.getConnection();
+            if (cp == null) { return p; }
+            using (OracleCommand cmd = cp.CreateCommand())
+            {
+                cp.Open();
+                cmd.CommandText = "SELECT COD_PAIS,DESCRIPCION FROM TB_PAIS WHERE STATUS= 'A'";
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    SelectListItem item = new SelectListItem
+                    {
+                        Text = reader.GetString(1),
+                        Value = reader.GetInt32(0).ToString()
+                    };
+                    p.Add(item);
+                }
+            }
+            return p;
+        }
+    }
+}
